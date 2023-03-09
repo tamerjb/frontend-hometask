0d056eb189d9ddf7bd501ccda9222c37b4cd762d
AmitGit217 <amit217@yandex.com>
2023-03-09 21:06:18 +0200

    chore: add page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Signup from "./view/pages/Signup";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      Hello World!
+      <Signup />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/view/pages/Signup.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Signup: React.FC = () => {
+  return <div className="signup" />;
+};
+
+export default Signup;
